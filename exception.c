c52269720077142a975d02eb00b41d9201cfd441
nrootconauto <nrootconauto@gmail.com>
2020-10-04 13:41:12 +0000

    Added exception.c(untested)

--- exception.c
+++ exception.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <exception.h>
 #include <pthread.h>
 #include <setjmp.h>
 #include <skinny_mutex.h>
@@ -10,11 +11,13 @@ struct mallocLL {
 };
 struct __exceptionAllocs;
 struct __exceptionBlock {
-	jmp_buf *from;
+	jmp_buf from;
 	struct __exceptionBlock *prev;
 	long threadsCount;
 	struct __exceptionAllocs **threadAllocs;
 	skinny_mutex_t mutex;
+	long typesCount;
+	const struct exceptionType **types;
 };
 struct __exceptionAllocs {
 	struct __exceptionBlock *from;
@@ -23,6 +26,11 @@ struct __exceptionAllocs {
 };
 __thread struct __exceptionBlock *currentExceptionBlock = NULL;
 __thread struct __exceptionAllocs *currentExceptionAllocs = NULL;
+__thread struct __exceptionAllocs *globalExceptionAllocs = NULL;
+
+__thread const struct exceptionType *currentExceptionType = NULL;
+__thread void *currentExceptionValue = NULL;
+
 static struct __exceptionAllocs *
 exceptionAllocsNew(struct __exceptionBlock *from) {
 	struct __exceptionAllocs *new = malloc(sizeof(struct __exceptionAllocs));
@@ -31,15 +39,24 @@ exceptionAllocsNew(struct __exceptionBlock *from) {
 	new->allocs = NULL;
 	return new;
 }
-static struct __exceptionAllocs *threadGetScope() {
-	if (currentExceptionAllocs == NULL)
-		currentExceptionAllocs = exceptionAllocsNew(NULL);
+static struct __exceptionAllocs **threadGetAllocs() {
+	if (currentExceptionAllocs == NULL) {
+		if (globalExceptionAllocs == NULL) {
+			struct __exceptionAllocs *new = malloc(sizeof(struct __exceptionAllocs));
+			new->from = NULL;
+			new->threadId = pthread_self();
+			new->allocs = NULL;
+			globalExceptionAllocs = new;
+		}
 
-	return currentExceptionAllocs;
+		currentExceptionAllocs = globalExceptionAllocs;
+	}
+
+	return &currentExceptionAllocs;
 }
 void *__malloc(unsigned long size) {
 	struct mallocLL *retVal = malloc(size + sizeof(struct mallocLL));
-	__auto_type current = threadGetScope();
+	__auto_type current = *threadGetAllocs();
 
 	if (current->allocs != NULL)
 		retVal->next = current->allocs;
@@ -72,7 +89,10 @@ void *__realloc(void *ptr, unsigned long newSize) {
 
 	return (void *)alloc + sizeof(sizeof(struct mallocLL));
 }
-static void exceptionAllocsDestroy(struct __exceptionAllocs *allocs) {
+static void exceptionAllocsDestroy(void *Allocs) {
+	struct __exceptionAllocs *allocs = Allocs;
+	if (allocs == NULL)
+		return;
 
 	__auto_type currentAllocs = allocs->allocs;
 	if (currentAllocs != NULL) {
@@ -87,55 +107,162 @@ static void exceptionAllocsDestroy(struct __exceptionAllocs *allocs) {
 
 	free(currentExceptionBlock);
 }
-void exceptionLeaveBlock(void *Block);
-void exceptionEnterBlock(void *typePtr, jmp_buf *from) {
+void exceptionLeaveBlock();
+jmp_buf *exceptionEnterBlock() {
 	__auto_type prev = currentExceptionBlock;
 	struct __exceptionBlock *newBlock = malloc(sizeof(struct __exceptionBlock));
 	newBlock->prev = prev;
-	newBlock->from = from;
 	skinny_mutex_init(&newBlock->mutex);
 
 	newBlock->threadsCount = 1;
 	newBlock->threadAllocs = malloc(1 * sizeof(struct __exceptionAllocs *));
 	newBlock->threadAllocs[0] = exceptionAllocsNew(newBlock);
+	newBlock->typesCount = 0;
+	newBlock->types = NULL;
 
 	currentExceptionBlock = newBlock;
-	currentExceptionAllocs = newBlock->threadAllocs[0];
-
-	pthread_cleanup_push(exceptionLeaveBlock, newBlock);
-	pthread_cleanup_pop(0);
+	*threadGetAllocs() = newBlock->threadAllocs[0];
 
 	if (prev != NULL) {
 		skinny_mutex_lock(&prev->mutex);
 
-		prev->threadAllocs =
-		    realloc(prev->threadAllocs,
-		            sizeof(struct __exceptionAllocs *) * ++prev->threadsCount); // prev->threadsCount incremented here
+		prev->threadAllocs = realloc(
+		    prev->threadAllocs,
+		    sizeof(struct __exceptionAllocs *) *
+		        ++prev->threadsCount); // prev->threadsCount incremented here
 		prev->threadAllocs[prev->threadsCount - 1] = newBlock->threadAllocs[0];
 
 		skinny_mutex_unlock(&prev->mutex);
 	}
+
+	return &currentExceptionBlock->from;
 }
-void exceptionLeaveBlock(void *Block) {
-	struct __exceptionBlock *block = Block;
-	__auto_type prev = block->prev;
+static void exceptionBlockDestroy(struct __exceptionBlock *block) {
+	free(block->types);
+	free(block->threadAllocs);
+	skinny_mutex_destroy(&block->mutex);
+	free(block);
+}
+void exceptionLeaveBlock() {
+	struct __exceptionBlock *Block = currentExceptionBlock;
+	__auto_type prev = Block->prev;
 
-	skinny_mutex_lock(&block->mutex);
+	skinny_mutex_lock(&Block->mutex);
 
-	for (int i = 0; i != block->threadsCount; i++) {
-		if (pthread_equal(pthread_self(), block->threadAllocs[i]->threadId)) {
-			exceptionAllocsDestroy(block->threadAllocs[0]);
+	for (int i = 0; i != Block->threadsCount; i++) {
+		if (pthread_equal(pthread_self(), Block->threadAllocs[i]->threadId)) {
+			exceptionAllocsDestroy(Block->threadAllocs[0]);
+			__auto_type ptr = &Block->threadAllocs[i];
+			memmove(ptr, ptr + 1,
+			        sizeof(struct __exceptionAllocs) * (Block->threadsCount - i - 1));
 		}
 	}
 
-	for (int i = 0; i != block->threadsCount; i++) {
-		pthread_cancel(block->threadAllocs[i]->threadId);
-		pthread_join(block->threadAllocs[i]->threadId, NULL);
+	if (--Block->threadsCount == 0)
+		exceptionBlockDestroy(Block);
+	else
+		skinny_mutex_unlock(&Block->mutex);
+
+	currentExceptionBlock = prev;
+}
+void exceptionFailBlock() {
+	__auto_type block = currentExceptionBlock;
+	assert(block != NULL);
+
+	skinny_mutex_lock(&block->mutex);
+	__auto_type waitForCount = block->threadsCount;
+	pthread_t waitFor[waitForCount];
+	for (int i = 0; i != waitForCount; i++)
+		waitFor[i] = block->threadAllocs[i]->threadId;
+	skinny_mutex_unlock(&block->mutex);
+
+	for (int i = 0; i != waitForCount; i++)
+		pthread_join(waitFor[i], NULL);
+
+	exceptionAllocsDestroy(threadGetAllocs());
+	currentExceptionBlock = currentExceptionBlock->prev;
+}
+struct __blockAndData {
+	struct __exceptionBlock *block;
+	void *data;
+	void *(*func)(void *);
+};
+static void exceptionThreadExit(void *unused) {
+	while (currentExceptionBlock != NULL)
+		exceptionLeaveBlock();
+
+	exceptionAllocsDestroy(threadGetAllocs());
+}
+static void *trySpawnForward(void *pair) {
+	struct __blockAndData *Pair = pair;
+
+	currentExceptionAllocs = exceptionAllocsNew(Pair->block);
+
+	currentExceptionBlock = Pair->block;
+	if (currentExceptionBlock != NULL) {
+		skinny_mutex_lock(&currentExceptionBlock->mutex);
+
+		__auto_type oldSize = Pair->block->threadsCount++;
+		Pair->block->threadAllocs =
+		    realloc(Pair->block->threadAllocs, Pair->block->threadsCount);
+		Pair->block->threadAllocs[oldSize] = currentExceptionAllocs;
+
+		skinny_mutex_unlock(&currentExceptionBlock->mutex);
+	} else {
+		globalExceptionAllocs = currentExceptionAllocs;
 	}
 
-	free(block->threadAllocs);
-	skinny_mutex_destroy(&block->mutex);
+	void *retVal;
+	pthread_cleanup_push(exceptionAllocsDestroy, exceptionThreadExit);
+	retVal = Pair->func(Pair->data);
+	pthread_cleanup_pop(1);
+
+	return retVal;
 }
-void trySpawn(void(*func)(void *),void *arg) {
- __
+void trySpawn(void *(*func)(void *), void *arg) {
+	struct __blockAndData pair;
+	pair.block = currentExceptionBlock;
+	pair.data = arg;
+	pair.func = func;
+
+	pthread_t thread;
+	pthread_create(&thread, NULL, trySpawnForward, &pair);
+}
+void exceptionThrow(const struct exceptionType *type, void *data) {
+	__auto_type block = currentExceptionBlock;
+	for (; block != NULL;) {
+		for (int i = 0; i != block->typesCount; i++) {
+			if (block->types[i] == type) {
+				currentExceptionType = type;
+				currentExceptionValue = data;
+				longjmp(block->from, 1);
+			}
+		}
+		exceptionLeaveBlock();
+		block = currentExceptionBlock;
+	}
+
+	// Couldn't find handler so exit
+	if (block == NULL)
+		pthread_exit(NULL);
+}
+void exceptionBlockAddCatchType(const struct exceptionType *type) {
+	assert(currentExceptionBlock != NULL);
+
+	__auto_type oldSize = currentExceptionBlock->typesCount++;
+	currentExceptionBlock->types =
+	    realloc(currentExceptionBlock->types,
+	            sizeof(struct exceptionType) * currentExceptionBlock->typesCount);
+	currentExceptionBlock->types[oldSize] = type;
+}
+void *exceptionValueByType(const struct exceptionType *type) {
+	void *retVal = NULL;
+	if (type == currentExceptionType) {
+		retVal = currentExceptionValue;
+
+		currentExceptionValue = NULL;
+		currentExceptionType = NULL;
+	}
+
+	return retVal;
 }
