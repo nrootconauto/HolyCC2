2080adfb9731d014d36e9be125ea0fcb893529f6
nrootconauto <nrootconauto@gmail.com>
2020-10-08 15:07:42 -0400

    Added tests for event pool that test for removal

--- tests/eventPoolTests.c
+++ tests/eventPoolTests.c
@@ -18,20 +18,29 @@ static void setThree(void *callValue, void *ptr) {
 	*(int *)ptr = 3;
 }
 void eventPoolTests() {
-	a = 0, b = 0, c = 0;
+	a = 0, b = 0, c = 0, aTwo = 0;
 	int a2 = 1, b2 = 2, c2 = 3;
 	__auto_type ep = eventPoolCreate();
 
-	eventPoolAdd(ep, "a", setOne, &a, NULL);
-	eventPoolAdd(ep, "a", setOne, &aTwo, NULL);
-	eventPoolAdd(ep, "b", setTwo, &b, NULL);
-	eventPoolAdd(ep, "c", setThree, &c, NULL);
+	__auto_type aEvent = eventPoolAdd(ep, "a", setOne, &a, NULL);
+	__auto_type aTwoEvent = eventPoolAdd(ep, "a", setOne, &aTwo, NULL);
+	__auto_type bEvent = eventPoolAdd(ep, "b", setTwo, &b, NULL);
+	__auto_type cEvent = eventPoolAdd(ep, "c", setThree, &c, NULL);
 
 	eventPoolTrigger(ep, "a", &a2);
 	eventPoolTrigger(ep, "b", &b2);
 	eventPoolTrigger(ep, "c", &c2);
 
 	assert(a == 1 && aTwo == 1 && b == 2 && c == 3);
+	eventPoolRemove(ep, aEvent);
+	eventPoolRemove(ep, bEvent);
+	eventPoolRemove(ep, cEvent);
+	a = 0, b = 0, c = 0, aTwo = 0;
+
+	eventPoolTrigger(ep, "a", &a2);
+	eventPoolTrigger(ep, "b", &b2);
+	eventPoolTrigger(ep, "c", &c2);
+	assert(a == 0 && aTwo == 1 && b == 0 && c == 0);
 
 	eventPoolDestroy(ep);
 }
