eeaf4b8a23f56d13aeb1629d7f52e8b2e2887f2a
nrootconauto@gmail.com <nrootconauto@gmail.com>
2020-12-31 19:21:51 +0000

    Added frameLayout.c/h. (UNTESTED).
    Signed-off-by: nrootconauto@gmail.com <nrootconauto@gmail.com>

---
+++ frameLayout.h
@@ -0,0 +1,8 @@
+#include <IR.h>
+struct frameEntry {
+		long offset;
+		struct IRVar var;
+};
+STR_TYPE_DEF(struct frameEntry, FrameEntry);
+STR_TYPE_FUNCS(struct frameEntry, FrameEntry);
+strFrameEntry IRComputeFrameLayout(graphNodeIR start);
