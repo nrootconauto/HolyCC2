b07fd19c15fe0cc9a658badf3caf83bf8219cbf3
nrootconauto@gmail.com <nrootconauto@gmail.com>
2021-01-04 14:29:09 +0000

    Parses all opcodes.
    Signed-off-by: nrootconauto@gmail.com <nrootconauto@gmail.com>

--- opcodesParser.c
+++ opcodesParser.c
@@ -35,6 +35,11 @@ struct opcodeTemplateArg {
 				OPC_TEMPLATE_ARG_REL8,
 				OPC_TEMPLATE_ARG_REL16,
 				OPC_TEMPLATE_ARG_REL32,
+				OPC_TEMPLATE_ARG_M8,
+				OPC_TEMPLATE_ARG_M16,
+				OPC_TEMPLATE_ARG_M32,
+				OPC_TEMPLATE_ARG_M64,
+				OPC_TEMPLATE_ARG_STI,
 		} type;
 		union {
 				uint64_t uint;
@@ -129,6 +134,17 @@ static int lexKeyword(strChar text,long *Pos,const char **keywords,long kwCount,
 		for(long i=kwCount-1;i>=0;i--) {
 				long res=strncmp(keywords[i], text+pos,strlen(keywords[i]));
 				if(res==0) {
+						//Check if all alnums,if so,ensure there is no alnum charactor after find
+						int isAllAlnum=1;
+						for(long i2=0;i2!=strlen(keywords[i]);i2++) {
+								if(!isalnum(keywords[i][i2])) {
+										isAllAlnum=0;
+										break;
+								}
+						}
+						if(isAllAlnum)
+								if(isalnum(text[pos+strlen(keywords[i])]))
+										continue;
 						*result=keywords[i];
 						*Pos=pos+strlen(keywords[i]);
 						return 1;
@@ -254,6 +270,11 @@ void parseOpcodeFile() {
 				"REL8",
 				"REL16",
 				"REL32",
+				"M8",
+				"M16",
+				"M32",
+				"M64",
+				"STI",
 		};
 		qsort(keywords, sizeof(keywords)/sizeof(*keywords), sizeof(*keywords), strcmp2);
 		
@@ -282,8 +303,11 @@ void parseOpcodeFile() {
 				assert(item);
 				if(item->type==LEXER_KW) {
 						if(0==strcmp(item->value.kw,"OPCODE")) {
-								struct lexerItem *name CLEANUP(lexerItemDestroy)=lexItem(text, &pos, keywords, kwCount);
-								assert(name->type==LEXER_WORD);
+								struct lexerItem *name CLEANUP(lexerItemDestroy)=malloc(sizeof(struct lexerItem));
+								name->type=LEXER_WORD;
+								pos=skipWhitespace(text, pos);
+								lexName(text, &pos, &name->value.name);
+
 								strOpcodeTemplate templates=NULL;
 								for(;;) {
 										if(item->type==LEXER_KW) {
@@ -314,6 +338,11 @@ void parseOpcodeFile() {
 												for(;;) {
 														long oldPos=pos;
 														struct lexerItem *item CLEANUP(lexerItemDestroy)=lexItem(text, &pos, keywords, kwCount);
+#define ARG_BY_NAME(arg,name) else if(0==strcmp(item->value.kw, name)) { \
+																		struct opcodeTemplateArg __arg; \
+																		__arg.type=arg; \
+																		template.args=strOpcodeTemplateArgAppendItem(template.args, __arg); \
+														} 
 														if(item->type==LEXER_INT) {
 																pos=oldPos;
 																goto opcodeFinish;
@@ -342,102 +371,39 @@ void parseOpcodeFile() {
 																										||0==strcmp(item->value.kw, "IW")
 																										||0==strcmp(item->value.kw, "ID")) {
 																		//Immediate literals set immediate type
-																} else if(0==strcmp(item->value.kw, "RM8")) {
-																		struct opcodeTemplateArg arg;
-																		arg.type=OPC_TEMPLATE_ARG_RM8;
-																		template.args=strOpcodeTemplateArgAppendItem(template.args, arg);
-																} else if(0==strcmp(item->value.kw, "RM16")) {
-																		struct opcodeTemplateArg arg;
-																		arg.type=OPC_TEMPLATE_ARG_RM16;
-																		template.args=strOpcodeTemplateArgAppendItem(template.args, arg);
-																} else if(0==strcmp(item->value.kw, "RM32")) {
-																		struct opcodeTemplateArg arg;
-																		arg.type=OPC_TEMPLATE_ARG_RM32;
-																		template.args=strOpcodeTemplateArgAppendItem(template.args, arg);
-																} else if(0==strcmp(item->value.kw, "RM64")) {
-																		struct opcodeTemplateArg arg;
-																		arg.type=OPC_TEMPLATE_ARG_RM64;
-																		template.args=strOpcodeTemplateArgAppendItem(template.args, arg);
-																} else if(0==strcmp(item->value.kw, "R8")) {
-																		struct opcodeTemplateArg arg;
-																		arg.type=OPC_TEMPLATE_ARG_R8;
-																		template.args=strOpcodeTemplateArgAppendItem(template.args, arg);
-																} else if(0==strcmp(item->value.kw, "CB")
-																										||0==strcmp(item->value.kw, "CW")
-																										||0==strcmp(item->value.kw, "CD")) {
-																} else if(0==strcmp(item->value.kw, "REL32")) {
-																		struct opcodeTemplateArg arg;
-																		arg.type=OPC_TEMPLATE_ARG_REL32;
-																		template.args=strOpcodeTemplateArgAppendItem(template.args, arg);
-																}else if(0==strcmp(item->value.kw, "REL16")) {
-																		struct opcodeTemplateArg arg;
-																		arg.type=OPC_TEMPLATE_ARG_REL16;
-																		template.args=strOpcodeTemplateArgAppendItem(template.args, arg);
-																}else if(0==strcmp(item->value.kw, "REL8")) {
-																		struct opcodeTemplateArg arg;
-																		arg.type=OPC_TEMPLATE_ARG_REL8;
-																		template.args=strOpcodeTemplateArgAppendItem(template.args, arg);
-																}else if(0==strcmp(item->value.kw, "R16")) {
-																		struct opcodeTemplateArg arg;
-																		arg.type=OPC_TEMPLATE_ARG_R16;
-																		template.args=strOpcodeTemplateArgAppendItem(template.args, arg);
-																} else if(0==strcmp(item->value.kw, "R32")) {
-																		struct opcodeTemplateArg arg;
-																		arg.type=OPC_TEMPLATE_ARG_R32;
-																		template.args=strOpcodeTemplateArgAppendItem(template.args, arg);
-																} else if(0==strcmp(item->value.kw, "R64")) {
-																		struct opcodeTemplateArg arg;
-																		arg.type=OPC_TEMPLATE_ARG_R64;
-																		template.args=strOpcodeTemplateArgAppendItem(template.args, arg);
-																} else if(0==strcmp(item->value.kw, "IMM8")) {
-																		struct opcodeTemplateArg arg;
-																		arg.type=OPC_TEMPLATE_ARG_SINT8;
-																		template.args=strOpcodeTemplateArgAppendItem(template.args, arg);
-																} else if(0==strcmp(item->value.kw, "IMM16")) {
-																		struct opcodeTemplateArg arg;
-																		arg.type=OPC_TEMPLATE_ARG_SINT16;
-																		template.args=strOpcodeTemplateArgAppendItem(template.args, arg);
-																} else if(0==strcmp(item->value.kw, "IMM32")) {
-																		struct opcodeTemplateArg arg;
-																		arg.type=OPC_TEMPLATE_ARG_SINT32;
-																		template.args=strOpcodeTemplateArgAppendItem(template.args, arg);
-																} else if(0==strcmp(item->value.kw, "IMM64")) {
-																		struct opcodeTemplateArg arg;
-																		arg.type=OPC_TEMPLATE_ARG_SINT64;
-																		template.args=strOpcodeTemplateArgAppendItem(template.args, arg);
-																} else if(0==strcmp(item->value.kw, "UIMM8")) {
-																		struct opcodeTemplateArg arg;
-																		arg.type=OPC_TEMPLATE_ARG_UINT8;
-																		template.args=strOpcodeTemplateArgAppendItem(template.args, arg);
-																} else if(0==strcmp(item->value.kw, "UIMM16")) {
-																		struct opcodeTemplateArg arg;
-																		arg.type=OPC_TEMPLATE_ARG_UINT16;
-																		template.args=strOpcodeTemplateArgAppendItem(template.args, arg);
-																} else if(0==strcmp(item->value.kw, "UIMM32")) {
-																		struct opcodeTemplateArg arg;
-																		arg.type=OPC_TEMPLATE_ARG_UINT32;
-																		template.args=strOpcodeTemplateArgAppendItem(template.args, arg);
-																} else if(0==strcmp(item->value.kw, "UIMM64")) {
-																		struct opcodeTemplateArg arg;
-																		arg.type=OPC_TEMPLATE_ARG_UINT64;
-																		template.args=strOpcodeTemplateArgAppendItem(template.args, arg);
-																} else if(0==strcmp(item->value.kw, "MOFFS32")) {
-																		struct opcodeTemplateArg arg;
-																		arg.type=OPC_TEMPLATE_ARG_MOFFS32;
-																		template.args=strOpcodeTemplateArgAppendItem(template.args, arg);
-																}else if(0==strcmp(item->value.kw, "MOFFS16")) {
-																		struct opcodeTemplateArg arg;
-																		arg.type=OPC_TEMPLATE_ARG_MOFFS16;
-																		template.args=strOpcodeTemplateArgAppendItem(template.args, arg);
-																}else if(0==strcmp(item->value.kw, "MOFFS8")) {
-																		struct opcodeTemplateArg arg;
-																		arg.type=OPC_TEMPLATE_ARG_MOFFS8;
-																		template.args=strOpcodeTemplateArgAppendItem(template.args, arg);
-																}  else if(0==strcmp(item->value.kw, "SREG")) {
-																		struct opcodeTemplateArg arg;
-																		arg.type=OPC_TEMPLATE_ARG_SREG;
-																		template.args=strOpcodeTemplateArgAppendItem(template.args, arg);
-																} else if(0==strcmp(item->value.kw, "!")) {
+																}else if(0==strcmp(item->value.kw, "CB")
+																								||0==strcmp(item->value.kw, "CW")
+																								||0==strcmp(item->value.kw, "CD")) {
+																}
+																ARG_BY_NAME(OPC_TEMPLATE_ARG_RM16, "RM8")
+																		ARG_BY_NAME(OPC_TEMPLATE_ARG_RM16, "RM16")
+																		ARG_BY_NAME(OPC_TEMPLATE_ARG_RM32, "RM32")
+																		ARG_BY_NAME(OPC_TEMPLATE_ARG_RM64, "RM64")
+																		ARG_BY_NAME(OPC_TEMPLATE_ARG_REL8, "REL8")
+																		ARG_BY_NAME(OPC_TEMPLATE_ARG_REL16, "REL32")
+																		ARG_BY_NAME(OPC_TEMPLATE_ARG_REL32, "REL16")
+																		ARG_BY_NAME(OPC_TEMPLATE_ARG_R8, "R8")
+																		ARG_BY_NAME(OPC_TEMPLATE_ARG_R16, "R16")
+																		ARG_BY_NAME(OPC_TEMPLATE_ARG_R32, "R32")
+																		ARG_BY_NAME(OPC_TEMPLATE_ARG_R64, "R64")
+																		ARG_BY_NAME(OPC_TEMPLATE_ARG_SINT8, "IMM8")
+																		ARG_BY_NAME(OPC_TEMPLATE_ARG_SINT16, "IMM16")
+																		ARG_BY_NAME(OPC_TEMPLATE_ARG_SINT32, "IMM32")
+																		ARG_BY_NAME(OPC_TEMPLATE_ARG_SINT64, "IMM64")
+																		ARG_BY_NAME(OPC_TEMPLATE_ARG_UINT8, "UIMM8")
+																		ARG_BY_NAME(OPC_TEMPLATE_ARG_UINT16, "UIMM16")
+																		ARG_BY_NAME(OPC_TEMPLATE_ARG_UINT32, "UIMM32")
+																		ARG_BY_NAME(OPC_TEMPLATE_ARG_UINT64, "UIMM64")
+																		ARG_BY_NAME(OPC_TEMPLATE_ARG_M8, "M8")
+																		ARG_BY_NAME(OPC_TEMPLATE_ARG_M16, "M16")
+																		ARG_BY_NAME(OPC_TEMPLATE_ARG_M32, "M32")
+																		ARG_BY_NAME(OPC_TEMPLATE_ARG_M64, "M64")
+																		ARG_BY_NAME(OPC_TEMPLATE_ARG_MOFFS32, "MOFFS32")
+																		ARG_BY_NAME(OPC_TEMPLATE_ARG_MOFFS16, "MOFFS16")
+																		ARG_BY_NAME(OPC_TEMPLATE_ARG_MOFFS8, "MOFFS8")
+																		ARG_BY_NAME(OPC_TEMPLATE_ARG_SREG, "SREG")
+																		ARG_BY_NAME(OPC_TEMPLATE_ARG_STI, "STI")
+																else if(0==strcmp(item->value.kw, "!")) {
 																} else if(0==strcmp(item->value.kw, "&")) {
 																} else if(0==strcmp(item->value.kw, "%")) {
 																} else if(0==strcmp(item->value.kw, "=")) {
